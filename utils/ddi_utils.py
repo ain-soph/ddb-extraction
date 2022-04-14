5585e6b5df9048075c2235e9cb312304ea96831a
Ren Pang <ain-soph@live.com>
2022-04-14 15:55:51 -0400

    update

--- utils/ddi_utils.py
+++ utils/ddi_utils.py
@@ -22,7 +22,7 @@ def read_str(data: io.BytesIO) -> str:
 
 def read_arr(data: io.BytesIO) -> bytes:
     assert data.read(4).decode() == 'ARR '
-    assert int.from_bytes(data.read(4), byteorder='little') == 0
+    int.from_bytes(data.read(4), byteorder='little')    # == 0   Exception: Tonio.ddi
     assert int.from_bytes(data.read(8), byteorder='little') == 1
     return data.read(4)
 
@@ -49,7 +49,18 @@ def read_ddi(ddi_bytes: bytes, dst_path: str,
     else:
         env['ddi_bytes'] = ddi_bytes
         ddi_data = io.BytesIO(ddi_bytes)
+        # DBSe
+        # Tonio.ddi has no DBSe block
+        
+        # assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0
+        # assert ddi_data.read(4).decode() == 'DBSe'
+        # assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+        # assert int.from_bytes(ddi_data.read(8), byteorder='little') == 1
+        # assert int.from_bytes(ddi_data.read(4), byteorder='little') == 3
+
         # PHDC
+        phdc_offset = ddi_bytes.find(b'PHDC')
+        ddi_data.seek(phdc_offset)
         phdc_data = read_phdc(ddi_data)
         if save_temp:
             with open(os.path.join(dst_path, 'phdc.yml'), mode='w',
@@ -64,7 +75,7 @@ def read_ddi(ddi_bytes: bytes, dst_path: str,
         tdb_data = read_tdb(ddi_data)
         if save_temp:
             with open(os.path.join(dst_path, 'tdb.yml'), mode='w',
-                      encoding='utf-8') as tdb_f:
+                    encoding='utf-8') as tdb_f:
                 tdb_str = yaml.dump(tdb_data, default_flow_style=False,
                                     sort_keys=False)
                 tdb_f.write(tdb_str)
@@ -171,13 +182,6 @@ def read_phdc(ddi_data: io.BytesIO):
                     | dict[str, list[str]]
                     | str]
     phdc_data = {}
-    # DBSe
-    assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0
-    assert ddi_data.read(4).decode() == 'DBSe'
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
-    assert int.from_bytes(ddi_data.read(8), byteorder='little') == 1
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 3
-
     # PHDC
     phoneme_data: dict[int, list[str]] = {0: [], 1: []}
     assert ddi_data.read(4).decode() == 'PHDC'
@@ -250,17 +254,15 @@ def read_tdb(ddi_data: io.BytesIO) -> dict[int, str]:
     tdb_data: dict[int, str] = {}
     assert ddi_data.read(8) == b'\xFF'*8
     assert ddi_data.read(4).decode() == 'TDB '
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 1
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+    int.from_bytes(ddi_data.read(4), byteorder='little')    # == 0 Exception: Tonio.ddi (B9 13 10 00)
+    assert int.from_bytes(ddi_data.read(8), byteorder='little') == 1
     tmm_num = int.from_bytes(ddi_data.read(4), byteorder='little')
     str_list = ['pitch', 'dynamics', 'opening']
     for i in range(tmm_num):
         assert ddi_data.read(8) == b'\xFF'*8
         assert ddi_data.read(4).decode() == 'TMM '
-        assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
-        assert int.from_bytes(ddi_data.read(4), byteorder='little') == 1
-        assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+        int.from_bytes(ddi_data.read(4), byteorder='little')    # == 0 Exception: Tonio.ddi
+        assert int.from_bytes(ddi_data.read(8), byteorder='little') == 1
         idx = int.from_bytes(ddi_data.read(4), byteorder='little')
         # print(i, idx)
         str_num = int.from_bytes(ddi_data.read(4), byteorder='little')
@@ -278,7 +280,7 @@ def read_tdb(ddi_data: io.BytesIO) -> dict[int, str]:
 def read_dbv(ddi_data: io.BytesIO) -> None:
     assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0
     assert ddi_data.read(4).decode() == 'DBV '
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+    int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
     assert int.from_bytes(ddi_data.read(8), byteorder='little') == 1
     int.from_bytes(ddi_data.read(4), byteorder='little')    # 4 for AVANNA, 5 for others?
 
@@ -290,15 +292,14 @@ def read_sta(ddi_data: io.BytesIO) -> dict[int, artu_type]:
     assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0
 
     assert ddi_data.read(4).decode() == 'STA '
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 1
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+    int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
+    assert int.from_bytes(ddi_data.read(8), byteorder='little') == 1
     stau_num = int.from_bytes(ddi_data.read(4), byteorder='little')
     for i in range(stau_num):
         stau_data: artu_type = {'phoneme': '', 'stap': {}}
         assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0
         assert ddi_data.read(4).decode() == 'STAu'
-        assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+        int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
         assert int.from_bytes(ddi_data.read(4), byteorder='little') == 1
         assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
         stau_idx = int.from_bytes(ddi_data.read(4), byteorder='little')
@@ -308,11 +309,12 @@ def read_sta(ddi_data: io.BytesIO) -> dict[int, artu_type]:
             stap_data: artp_type = {'snd': '', 'snd_unknown': '', 'epr': []}
             assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0
             assert ddi_data.read(4).decode() == 'STAp'
-            assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+            int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
             assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
             assert int.from_bytes(ddi_data.read(4), byteorder='little') == 1
             stap_data['unknown1'] = bytes_to_str(ddi_data.read(0x12))
-            assert ddi_data.read(8) == b'\x00\x00\x00\x00\x9A\x99\x19\x3F'
+            int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi (0x19880)
+            assert ddi_data.read(4) == b'\x9A\x99\x19\x3F'
             unknown = bytes_to_str(ddi_data.read(4))
             # print(f'sta {i:4d} {j:4d} {unknown}')
             # if env['unknown'] is None:
@@ -323,15 +325,15 @@ def read_sta(ddi_data: io.BytesIO) -> dict[int, artu_type]:
             assert int.from_bytes(ddi_data.read(4), byteorder='little') == 2
             assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0x3D
             assert ddi_data.read(4).decode() == 'EMPT'
-            assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+            int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
             assert read_str(ddi_data) == 'SND'
             unknown_snd = int.from_bytes(ddi_data.read(4), byteorder='little')
             stap_data['snd_unknown'] = f'{unknown_snd:08x}'
             assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
             assert ddi_data.read(4).decode() == 'EMPT'
-            assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+            int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
             assert read_str(ddi_data) == 'EpR'
-            assert ddi_data.read(4) == b'\xFF'*4
+            assert ddi_data.read(4)  # == b'\xFF'*4  Exception: Tonio.ddi (epr_num)
             epr_num = int.from_bytes(ddi_data.read(4), byteorder='little')
             epr_list: list[str] = []
             for k in range(epr_num):
@@ -364,7 +366,7 @@ def read_sta(ddi_data: io.BytesIO) -> dict[int, artu_type]:
 
 def read_art(ddi_data: io.BytesIO) -> dict[int, art_type]:
     total_art_data: dict[int, art_type] = {}
-    assert int.from_bytes(ddi_data.read(8), byteorder='little') == 0
+    int.from_bytes(ddi_data.read(8), byteorder='little')  # == 0 Exception: Tonio.ddi
     assert int.from_bytes(read_arr(ddi_data), byteorder='little') != 0
     while(True):
         start = ddi_data.read(8)
@@ -383,7 +385,7 @@ def read_art(ddi_data: io.BytesIO) -> dict[int, art_type]:
 
 def read_art_block(ddi_data: io.BytesIO) -> tuple[int, art_type]:
     art_data: art_type = {'phoneme': '', 'artu': {}, 'art': {}}
-    assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+    int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
     assert int.from_bytes(ddi_data.read(4), byteorder='little') == 1
     assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
     art_idx = int.from_bytes(ddi_data.read(4), byteorder='little')
@@ -399,7 +401,7 @@ def read_art_block(ddi_data: io.BytesIO) -> tuple[int, art_type]:
             continue
         else:
             assert block_type == 'ARTu'
-        assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+        int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
         assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
         assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
         artu_idx = int.from_bytes(ddi_data.read(4), byteorder='little')
@@ -412,7 +414,7 @@ def read_art_block(ddi_data: io.BytesIO) -> tuple[int, art_type]:
             artp_data: artp_type = {'snd': '', 'snd_unknown': '', 'epr': []}
             artp_data['unknown0'] = bytes_to_str(ddi_data.read(8))
             assert ddi_data.read(4).decode() == 'ARTp'
-            assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+            int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
             assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
             assert int.from_bytes(ddi_data.read(4), byteorder='little') == 1
             artp_data['unknown1'] = bytes_to_str(ddi_data.read(0x12))
@@ -427,23 +429,38 @@ def read_art_block(ddi_data: io.BytesIO) -> tuple[int, art_type]:
             # TODO: This doesn't seem to be an index actually
             artp_idx = int.from_bytes(ddi_data.read(8), byteorder='little')
             assert ddi_data.read(4).decode() == 'EMPT'
-            assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+            int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
             assert read_str(ddi_data) == 'SND'
             unknown_snd = int.from_bytes(ddi_data.read(4), byteorder='little')
             artp_data['snd_unknown'] = f'{unknown_snd:08x}'
             assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
             assert ddi_data.read(4).decode() == 'EMPT'
-            assert int.from_bytes(ddi_data.read(4), byteorder='little') == 0
+            int.from_bytes(ddi_data.read(4), byteorder='little')  # == 0 Exception: Tonio.ddi
             assert read_str(ddi_data) == 'EpR'
-            epr_num = int.from_bytes(ddi_data.read(4), byteorder='little')
-            epr_list: list[str] = []
-            for k in range(epr_num):
-                epr_offset = int.from_bytes(ddi_data.read(8),
-                                            byteorder='little')
-                epr_list.append(f'{epr_offset:0>8x}')
-            artp_data['epr'] = epr_list
-            assert ddi_data.read(4) == b'\x44\xAC\x00\x00'
-            assert ddi_data.read(2) == b'\x01\x00'
+            loc = ddi_data.tell()
+            try:
+                epr_num = int.from_bytes(ddi_data.read(4), byteorder='little')
+                epr_list: list[str] = []
+                for k in range(epr_num):
+                    epr_offset = int.from_bytes(ddi_data.read(8),
+                                                byteorder='little')
+                    epr_list.append(f'{epr_offset:0>8x}')
+                artp_data['epr'] = epr_list
+                assert ddi_data.read(4) == b'\x44\xAC\x00\x00'
+                assert ddi_data.read(2) == b'\x01\x00'
+            except AssertionError:
+                ddi_data.seek(loc)
+                assert ddi_data.read(4)  # == b'\xFF'*4  Exception: Tonio.ddi (epr_num)
+                epr_num = int.from_bytes(ddi_data.read(4), byteorder='little')
+                epr_list: list[str] = []
+                for k in range(epr_num):
+                    epr_offset = int.from_bytes(ddi_data.read(8),
+                                                byteorder='little')
+                    epr_list.append(f'{epr_offset:0>8x}')
+                artp_data['epr'] = epr_list
+                assert ddi_data.read(4) == b'\x44\xAC\x00\x00'
+                assert ddi_data.read(2) == b'\x01\x00'
+                
             snd_identifier = int.from_bytes(ddi_data.read(4),
                                             byteorder='little')
             # TODO: why this number?
@@ -451,7 +468,7 @@ def read_art_block(ddi_data: io.BytesIO) -> tuple[int, art_type]:
             artp_data['snd'] = f'{snd_offset-0x12:016x}_{snd_identifier:08x}'
             assert int.from_bytes(ddi_data.read(8),
                                   byteorder='little'
-                                  ) == snd_offset+0x800
+                                  )  # == snd_offset+0x800  Exception: Tonio.ddi (0)
 
             ddi_bytes: bytes = env['ddi_bytes'][ddi_data.tell():]
             unknown2_length = ddi_bytes.find(b'default')-4
